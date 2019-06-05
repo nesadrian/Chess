24abb7abb10f0e9fb99b86aee56a3b1f088f20e1
Adrian Nesvik <adrian.nesvik@lyse.net>
2019-06-06 00:28:52 +0200

    Added display of total game moves

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -3,8 +3,8 @@ package chess;
 public class Chess {
 
     public static void main(String[] args) {
-        Player newPlayer = new Player();
-        newPlayer.startGame();
+        GameController newGame = new GameController();
+        newGame.startGame();
     }
     
 }
--- src/chess/GameController.java
+++ src/chess/GameController.java
@@ -1,32 +1,55 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Player {
+public class GameController {
     
     boolean isWhiteTurn;
     ChessBoard newBoard;
     Scanner playerInput;
+    ArrayList<String> moveList;
     
-    public Player() {
+    public GameController() {
         isWhiteTurn = true;
         newBoard = new ChessBoard();
         playerInput = new Scanner(System.in);
+        moveList = new ArrayList();
     }
     
     public void startGame() {
         System.out.println("This program uses algebraic chess notation as input to determine moves.");
-        System.out.println("The input response will vary depending on the legality of your move.");
+        System.out.println("The response from input your will vary depending on the legality of your move.");
         System.out.println("---------------------------------------------------------------------");
         System.out.println("White player's turn");
         String nextMove = playerInput.nextLine();
         playerTurn(nextMove);
-        
     }
     
     public void playerTurn(String move) {
         checkMoveLegality(move);
         isWhiteTurn = !isWhiteTurn;
+        moveList.add(move);
+        printRoundInfo();
+        String nextMove = playerInput.nextLine();
+        playerTurn(nextMove);
+    }
+    
+    public boolean checkMoveLegality(String move) {
+        return true;
+    }
+    
+    public void printRoundInfo() {
+        System.out.print("-------------------");
+        for(int i = 0; i < moveList.size(); i++) {
+            if(i % 2 == 0) {
+                System.out.println("");
+                System.out.print((i/2) + 1 + ". ");
+            }
+            System.out.print(moveList.get(i) + " ");
+        }
+        System.out.println("");
+        System.out.println("-------------------");
         
         if (isWhiteTurn) {
             System.out.println("White player's turn");
@@ -34,12 +57,5 @@ public class Player {
         else if (!isWhiteTurn) {
             System.out.println("Black player's turn");
         }
-        String nextMove = playerInput.nextLine();
-        playerTurn(nextMove);
-    }
-    
-    public boolean checkMoveLegality(String move) {
-        
-        return true;
     }
 }
